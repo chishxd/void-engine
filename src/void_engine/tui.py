35328cf9aec15276c3615120209300a4c2a54e66
Chinmay Shet <chishxd@gmail.com>
2025-10-30 20:38:22 +0530

    Define basic TUI

--- src/void_engine/tui.py
+++ src/void_engine/tui.py
@@ -1,7 +1,17 @@
-def main():
-    print("Hello from void-engine!")
-    print("Hello")
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+
+
+class VoidApp(App):
+    TITLE = "V.O.I.D Engine v1.0"
+    SUB_TITLE = "The powers from future.. in your terminal!"
+    BINDINGS = [("q", "exit_app", "Exit")]
+
+    def compose(self) -> ComposeResult:
+        yield Header()
+        yield Footer()
 
 
 if __name__ == "__main__":
-    main()
+    app = VoidApp()
+    app.run()
