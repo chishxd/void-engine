2b0aaa931b143c1374b02b93037961849e3a4e05
Chinmay Shet <chishxd@gmail.com>
2025-10-30 22:22:24 +0530

    Add CommandPanel Widget

--- src/void_engine/tui.py
+++ src/void_engine/tui.py
@@ -1,8 +1,18 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header
+from textual.widgets import Footer, Header, Static
+
+from void_engine.config import COMMANDS
 
 
-class VoidApp(App):
+class CommandPanel(Static):
+    def on_mount(self) -> None:
+        commands: str = ""
+        for cmd in COMMANDS:
+            commands += f"\n{cmd} ----- {COMMANDS[cmd]}\n"
+        self.update(commands)
+
+
+class VoidApp(App[None]):
     TITLE = "V.O.I.D Engine v1.0"
     SUB_TITLE = "The powers from future.. in your terminal!"
     BINDINGS = [("q", "exit_app", "Exit")]
@@ -10,6 +20,9 @@ class VoidApp(App):
     def compose(self) -> ComposeResult:
         yield Header()
         yield Footer()
+        yield CommandPanel(id="command-panel")
+        yield Static("Visualizer")
+        yield Static("Logs")
 
 
 if __name__ == "__main__":
