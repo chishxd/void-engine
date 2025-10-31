2c27386830b8ce46e48cee37ed04d6ed381eff20
Chinmay Shet <chishxd@gmail.com>
2025-10-31 23:40:50 +0530

    Update input box
    Input box also vanishes once you type 'awaken' also adds logs

--- src/void_engine/tui.py
+++ src/void_engine/tui.py
@@ -5,6 +5,7 @@ from textual.widgets import Footer, Header, Input, Static
 from void_engine.config import COMMANDS
 
 
+# The widget for all commands
 class CommandPanel(Static):
     def on_mount(self) -> None:
         commands: str = ""
@@ -13,12 +14,20 @@ class CommandPanel(Static):
         self.update(commands)
 
 
+# The main container
 class VoidApp(App[None]):
     TITLE = "V.O.I.D Engine v1.0"
     SUB_TITLE = "The powers from future.. in your terminal!"
     BINDINGS = [("q", "exit_app", "Exit")]
 
     CSS_PATH = "tui.tcss"
+
+    is_true: bool = False
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.is_awake: bool = False
 
     def compose(self) -> ComposeResult:
         yield Header()
@@ -32,11 +41,17 @@ class VoidApp(App[None]):
                     Static("VISUALIZER", id="visualizer_panel"),
                     Static("LOGS", id="logs_panel"),
                 ),
-                id="main_content"
+                id="main_content",
             ),
             Input(placeholder="type 'awaken' and press Enter", id="command_input"),
-            id="app_grid"
+            id="app_grid",
         )
+
+    def on_input_submitted(self, event: Input.Submitted):
+        if event.value.lower() == "awaken":
+            self.is_awake: bool = True
+            self.query_one("#logs_panel", Static).update("[LOG] > Engine AWAKENED")
+            self.query("#command_input").remove()
 
 
 if __name__ == "__main__":
