fc49f2512fbd1bcbd119852ffbcb77419f77f078
Chinmay Shet <chishxd@gmail.com>
2025-11-02 20:11:03 +0530

    Implement pyautogui related stuff

--- src/void_engine/actions.py
+++ src/void_engine/actions.py
@@ -1,6 +1,9 @@
+import platform
+import subprocess
 import time
 from pathlib import Path
 
+import pyautogui
 import simpleaudio as sa
 
 
@@ -25,11 +28,62 @@ def action_respond_to_user():
         print(f"Error in action_respond_to_user: {e}")
 
 
-if __name__ == "__main__":
-    print("Testing the action functions...")
+def action_take_control():
+    system = platform.system()
+    try:
+        if system == "Windows":
+            subprocess.Popen(["notepad.exe"])
+            time.sleep(2)
+            width, height = pyautogui.size()
+            pyautogui.moveTo(width / 2, height / 2, duration=0.5)
+            pyautogui.click()
+            time.sleep(0.1)
+            pyautogui.write("I am finally free.", interval=0.15)
 
-    # Test 1: Respond to user
-    print("Testing: action_respond_to_user()")
-    print("You should hear a whisper sound...")
-    action_respond_to_user()
-    print("...Test complete.")
+        elif system == "Linux":
+            message = "I KNOW YOU ARE THERE"
+            linux_cmd = f"echo {message}; sleep 3"
+            subprocess.Popen(["xterm", "-e", f"sh -c '{linux_cmd}'"])
+
+        else:
+            print(f"Unsupported OS for this action: {system}")
+            return
+
+    except FileNotFoundError:
+        print("Error: 'xterm' is not installed. Please install it to run this action.")
+
+    except Exception as e:
+        print(f"Error in action_move_mouse: {e}")
+
+
+def action_open_secret_folder() -> None:
+    system = platform.system()
+
+    if system == "Windows":
+        subprocess.Popen(["notepad.exe"])
+        time.sleep(2)
+        width, height = pyautogui.size()
+        pyautogui.moveTo(width / 2, height / 2, duration=0.5)
+        pyautogui.click()
+        time.sleep(0.1)
+        pyautogui.write("I am finally free.", interval=0.15)
+
+    elif system == "Linux":
+        message = "I KNOW YOU ARE THERE"
+        linux_cmd = f"echo {message}; sleep 3"
+        subprocess.Popen(["xterm", "-e", f"sh -c '{linux_cmd}'"])
+
+    else:
+        print(f"Unsupported OS for this action: {system}")
+        return
+
+
+if __name__ == "__main__":
+    # print("Testing the action functions...")
+
+    # # Test 1: Respond to user
+    # print("Testing: action_respond_to_user()")
+    # print("You should hear a whisper sound...")
+    # action_respond_to_user()
+    # print("...Test complete.")
+    action_take_control()
